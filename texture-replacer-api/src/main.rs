2dc49442788395e46c2e9cccab393853a7eb3367
William <drewlikesramen@gmail.com>
2022-10-31 20:30:58 -0400

    Creating basic rockit app

--- texture-replacer-api/src/main.rs
+++ texture-replacer-api/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{routes, launch, get, post};
+
+
+#[get("/health")]
+fn health() -> String {
+    String::from("RUNNING")
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![health])
 }
